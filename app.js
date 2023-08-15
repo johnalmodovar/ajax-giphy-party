975121722957a2cd0cd24156c8d772911de06f72
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-08-15 16:25:20 -0700

    started on async function to grab our gif

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Let's get this party started!");
+"use strict";
+
+/** async function to grab our gif from api database */
+
+async function grabGif() {
+  const gifName = $(".search-id").val();
+  console.log("gifName", gifName)
+
+  const response = await fetch(
+    `api.giphy.com/v1/gifs/search`,
+    {
+      method: "POST",
+      body: JSON.stringify({gifName}),
+      headers: { "Content-Type": "application/json" }
+    });
+}
